07336bed7467f5f81cf5294b1fc3285b2af74e55
unknown <stnqls3938@gmail.com>
2021-09-28 17:01:24 +0900

    구글 로그인 만들기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { authService, firebaseInstance } from "./fBase";
 
-function App() {
+const App = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true); // 새로운 유저인지 확인(초기값: true)
 
   const onChange = (event) => {
     const {
@@ -14,8 +16,43 @@ function App() {
       setPassword(value);
     }
   };
-  const onSubmit = (event) => {
+
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        // create account
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        // login
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+
+  const onGoogleClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
+
+  const onLogOutClick = () => {
+    authService.signOut();
   };
 
   return (
@@ -37,10 +74,19 @@ function App() {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Login" />
+        <input type="submit" value={newAccount ? "Create Account" : "Login"} />
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Login" : "Create Account"}
+      </span>
+      <button name="google" onClick={onGoogleClick}>
+        구글 계정 로그인
+      </button>
+      <button name="googleLogOut" onClick={onLogOutClick}>
+        로그아웃
+      </button>
     </div>
   );
-}
+};
 
 export default App;
