a37ef6c0998dc51ce743f27a32b93e812a974692
unknown <stnqls3938@gmail.com>
2021-09-29 18:05:58 +0900

    데이터-PostList map

--- src/pages/post/PostList.jsx
+++ src/pages/post/PostList.jsx
@@ -1,49 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CommonTable from "../../components/CommonTable";
 import CommonTableColumn from "../../components/CommonTableColumn";
 import CommonTableRow from "../../components/CommonTableRow";
+import { postList } from "../../components/Data";
 
 const PostList = (props) => {
+  const [dataList, setDataList] = useState([]);
+
+  useEffect(() => {
+    setDataList(postList);
+  }, []);
+
   return (
     <React.Fragment>
       <CommonTable
         headersName={["글번호", "제목", "등록일", "조회수", "추천수"]}
       >
-        <CommonTableRow>
-          <CommonTableColumn>1</CommonTableColumn>
-          <CommonTableColumn>1번 게시물</CommonTableColumn>
-          <CommonTableColumn>2021-09-29</CommonTableColumn>
-          <CommonTableColumn>10</CommonTableColumn>
-          <CommonTableColumn>3</CommonTableColumn>
-        </CommonTableRow>
-        <CommonTableRow>
-          <CommonTableColumn>2</CommonTableColumn>
-          <CommonTableColumn>2번 게시물</CommonTableColumn>
-          <CommonTableColumn>2021-09-29</CommonTableColumn>
-          <CommonTableColumn>10</CommonTableColumn>
-          <CommonTableColumn>3</CommonTableColumn>
-        </CommonTableRow>
-        <CommonTableRow>
-          <CommonTableColumn>3</CommonTableColumn>
-          <CommonTableColumn>3번 게시물</CommonTableColumn>
-          <CommonTableColumn>2021-09-29</CommonTableColumn>
-          <CommonTableColumn>10</CommonTableColumn>
-          <CommonTableColumn>3</CommonTableColumn>
-        </CommonTableRow>
-        <CommonTableRow>
-          <CommonTableColumn>4</CommonTableColumn>
-          <CommonTableColumn>4번 게시물</CommonTableColumn>
-          <CommonTableColumn>2021-09-29</CommonTableColumn>
-          <CommonTableColumn>10</CommonTableColumn>
-          <CommonTableColumn>3</CommonTableColumn>
-        </CommonTableRow>
-        <CommonTableRow>
-          <CommonTableColumn>5</CommonTableColumn>
-          <CommonTableColumn>5번 게시물</CommonTableColumn>
-          <CommonTableColumn>2021-09-29</CommonTableColumn>
-          <CommonTableColumn>10</CommonTableColumn>
-          <CommonTableColumn>3</CommonTableColumn>
-        </CommonTableRow>
+        {dataList
+          ? dataList.map((item, index) => {
+              return (
+                <CommonTableRow key={index}>
+                  <CommonTableColumn>{item.no}</CommonTableColumn>
+                  <CommonTableColumn>{item.title}</CommonTableColumn>
+                  <CommonTableColumn>{item.createDate}</CommonTableColumn>
+                  <CommonTableColumn>{item.readCount}</CommonTableColumn>
+                  <CommonTableColumn>
+                    {item.recommandationCount}
+                  </CommonTableColumn>
+                </CommonTableRow>
+              );
+            })
+          : ""}
       </CommonTable>
     </React.Fragment>
   );
